025bacb7d7030c16bd06ef98f50fe723a2bcb367
Fjgl96 <131479390+Fjgl96@users.noreply.github.com>
2025-12-06 18:47:29 -0500

    Cambios

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/chat', // Cuando el front llame a /chat
+        destination: 'http://127.0.0.1:8080/chat', // Next.js lo manda al backend
+      },
+    ]
+  },
 };
 
 export default nextConfig;
